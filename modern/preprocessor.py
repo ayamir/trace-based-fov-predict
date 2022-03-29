57145eae1a084b7adf8c56b31267bbfcd47a0da6
ayamir <lgt986452565@gmail.com>
2022-03-29 17:05:30 +0800

    feat: add data tidy.

--- modern/preprocessor.py
+++ modern/preprocessor.py
@@ -1,15 +1,102 @@
+import re
 import pandas as pd
 import os
 import cmder
 
-SPLIT_PATH = "../dataset/shanghai_gaze/train_test_set.xlsx"
-SET_PATH = "../dataset/shanghai_gaze/"
+from pathlib import Path
+
+SPLIT_PATH = "../../dataset/shanghai_gaze/train_test_set.xlsx"
+SET_PATH = "../../dataset/shanghai_gaze/"
+
+DATA = "../../dataset/xr_data/"
+DATA1 = "../../dataset/xr_data/Experiment_1/"
+DATA2 = "../../dataset/xr_data/Experiment_2/"
 
 HW = 1
 PW = 1
 FPS = 30
 DOWNSAMPLE = 2
-MODELS_PATH = "./hw" + str(HW) + "pw" + str(PW) + "models/"
+MODELS_PATH = "../../models/modern/hw" + str(HW) + "pw" + str(PW) + "models/"
+
+
+category_dict = {
+    "Film": ["2"],
+    "Talkshow": ["11", "17"],
+    "Documentary": ["5", "8"],
+    "Sport": ["1", "4", "7", "12", "13"],
+    "Performance": ["0", "3", "6", "9", "10", "14", "15", "16"],
+}
+
+
+def align_filename():
+    cmder.infOut("Aligning filename...")
+    files = list(Path(DATA2).rglob("video_*.csv"))
+    for file in files:
+        file = str(file)
+        old_no = file[-5:][0]
+        new_no = str(eval(old_no) + 9)
+        new_file = new_no.join(file.rsplit(old_no, 1))
+        os.system(f"mv {file} {new_file}")
+    cmder.successOut("Aligning finished.")
+
+
+def merge_data():
+    cmder.infOut("Merging data...")
+    files = list(Path(DATA).rglob("video_*.csv"))
+    for file in files:
+        file = str(file)
+        dir = file.rsplit("/", 1)[0]
+        new_dir = DATA + dir.rsplit("/", 1)[1]
+        if not os.path.isdir(new_dir):
+            os.system(f"mv {dir} {new_dir}")
+        else:
+            os.system(f"mv {dir}/* {new_dir}")
+    os.system(f"rm -rf {DATA1} {DATA2}")
+    cmder.successOut("Merge finished.")
+
+
+def classify():
+    files = []
+    filepaths = list(Path(DATA).rglob("video_*.csv"))
+    for file in filepaths:
+        file = str(file)
+        files.append(file)
+
+    cmder.infOut("Classifying data...")
+    for category in category_dict:
+        category_dir = DATA + category
+        if not os.path.isdir(category_dir):
+            os.system(f"mkdir -p {category_dir}")
+        nos = category_dict[category]
+        category_datas = []
+        for no in nos:
+            data_name = ".*video_" + no + ".csv"
+            r = re.compile(data_name)
+            filtered_data = list(filter(r.match, files))
+            category_datas += filtered_data
+        for category_data in category_datas:
+            new_data = category_data[len(DATA) :].replace("/", "_")
+            os.system(f"cp {category_data} {category_dir}/{new_data}")
+    cmder.successOut("Classify finished.")
+
+
+def datatidy():
+    align_filename()
+    merge_data()
+    classify()
+
+
+def count_data_num():
+    cnt = 0
+    cmder.infOut("Counting records number...")
+    files = list(Path(DATA).rglob("video_*.csv"))
+    for file in files:
+        file = str(file)
+        argu = "'{print $1}'"
+        _, res = cmder.runCmd(f"wc -l {file} | awk {argu}")
+        res = eval(res)
+        cnt += res
+    cmder.successOut("Records number = " + str(cnt))
 
 
 class Set:
@@ -82,7 +169,4 @@ def create_dataset(cls: str):
 
 
 if __name__ == "__main__":
-    if not os.path.isdir(MODELS_PATH):
-        cmder.runCmd(f"mkdir -p {MODELS_PATH}")
-    create_dataset("train")
-    create_dataset("test")
+    datatidy()
